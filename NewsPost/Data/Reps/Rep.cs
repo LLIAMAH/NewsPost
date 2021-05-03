f2e4e30d3b16399bd3649f129592af58f9af0d96
Vasily Himchenko <White_Scorpion@inbox.lv>
2021-05-03 15:43:14 +0300

    Reps added.

---
+++ NewsPost/Data/Reps/Rep.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NewsPost.Data.Reps
+{
+    public partial class Rep : IRepUsers
+    {
+        private readonly AppDbCtx _ctx;
+        private readonly ILogger<AppDbCtx> _logger;
+
+        public Rep(AppDbCtx ctx, ILogger<AppDbCtx> logger)
+        {
+            this._ctx = ctx;
+            this._logger = logger;
+        }
+    }
+}
---
+++ NewsPost/Data/Reps/RepUsers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+using Microsoft.Extensions.Logging;
+using NewsPost.Data.Entities;
+using NewsPost.Data.Log;
+
+namespace NewsPost.Data.Reps
+{
+    public partial class Rep
+    {
+        public ApplicationUser GetUser(IPrincipal principal)
+        {
+            var name = MethodBase.GetCurrentMethod()?.Name;
+
+            try
+            {
+                _logger?.LogInformation(LogInfo.CreateLogStart(name));
+                var principalName = principal.Identity?.Name;
+                return _ctx.Users.SingleOrDefault(o => o.UserName.Equals(principalName));
+            }
+            catch (Exception ex)
+            {
+                this._logger?.LogError(LogInfo.CreateLog(name, ex));
+            }
+            finally
+            {
+                _logger?.LogInformation(LogInfo.CreateLogFinish(name));
+            }
+
+            return default;
+        }
+
+        public string GetUserId(IPrincipal principal)
+        {
+            var name = MethodBase.GetCurrentMethod()?.Name;
+
+            try
+            {
+                _logger?.LogInformation(LogInfo.CreateLogStart(name));
+                var user = GetUser(principal);
+                return user.Id;
+            }
+            catch (Exception ex)
+            {
+                this._logger?.LogError(LogInfo.CreateLog(name, ex));
+            }
+            finally
+            {
+                _logger?.LogInformation(LogInfo.CreateLogFinish(name));
+            }
+
+            return default;
+        }
+    }
+}
