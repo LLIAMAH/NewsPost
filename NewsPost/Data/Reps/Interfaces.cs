23cfdefd8bbca3033ecdea15af07a32def71bf3f
Vasily Himchenko <White_Scorpion@inbox.lv>
2021-05-05 11:10:13 +0300

    Logs added. Interfaces added.

---
+++ NewsPost/Data/Reps/Interfaces.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+using NewsPost.Data.Entities;
+
+namespace NewsPost.Data.Reps
+{
+    public interface IRep: IRepUser { }
+
+    public interface IRepUser
+    {
+        ApplicationUser GetUser(IPrincipal principal);
+        string GetUserId(IPrincipal principal);
+    }
+}
