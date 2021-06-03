82fb054032733250cc98c20fef71617a73a3503b
Vasily Himchenko <White_Scorpion@inbox.lv>
2021-06-04 01:28:57 +0300

    Removed unrequired usings

--- NewsPost/Areas/Identity/IdentityHostingStartup.cs
+++ NewsPost/Areas/Identity/IdentityHostingStartup.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using NewsPost.Data;
-using NewsPost.Data.Entities;
+﻿using Microsoft.AspNetCore.Hosting;
 
 [assembly: HostingStartup(typeof(NewsPost.Areas.Identity.IdentityHostingStartup))]
 namespace NewsPost.Areas.Identity
